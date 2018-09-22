bad9145d6a114d74c3bf3dfa31de519bad5c6807
daniel.ospinap <daniel.ospinaperez@gmail.com>
2018-09-22 11:42:46 -0500

    get nombre

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <title>Prueba de daniel</title>
 </head>
 <body>
-<?php echo '<p>Hola mundo</p>'; ?>
+<?php 
+//$parts = parse_url($url);
+//parse_str($parts['query'], $query);
+//echo $query['nombre'];
+echo '<p>Hola mundo $_GET['nombre'] </p>'; 
+
+?>
 </body>
 </html>
